740f7ffd4aa266facb1e0a15d6d6cb3af3bf329d
Usha <usha.macdonald@gmail.com>
2017-08-03 20:45:23 +1200

    new pics

--- client/components/Flatties.jsx
+++ client/components/Flatties.jsx
@@ -5,7 +5,7 @@ const Flatties = ({flatties}) => {
   const renderFlattie = ({id, name, description, at_home, image}, key) => (
     <div key={id} className="flattie">
       <h1>{name}</h1>
-      <img src={image} />
+      <img className="flattie_pic" src={image} />
       <p className="flattieDescr">{description}</p>
     </div>
   )
