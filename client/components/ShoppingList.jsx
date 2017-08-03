7ac13c8729bd24aef21e24e991e3028d9e0e9387
Usha <usha.macdonald@gmail.com>
2017-08-04 09:43:09 +1200

    new branch

--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -4,14 +4,16 @@ import {getShopping} from '../api/api'
 import ShoppingListAdd from './ShoppingListAdd'
 
 export default function ShoppingList ({shoppingList, fetchShopping}) {
-  const renderShopping = ({id, item}) => (
-    <li key={id}>{item}</li>
+  const renderShopping = ({id, item, done}) => (
+    <li className="shopping_li" key={id}><input type="checkbox" name="check" value="1"/>
+             <label className="strikethrough">{item}</label>
+    </li>
   )
 
   return (
     <div className="shoppingContainer">
       <h2>Shopping List</h2>
-      <ul>
+      <ul className="shopping_list">
         {shoppingList.map(renderShopping)}
       </ul>
       <ShoppingListAdd fetchShopping={fetchShopping} />
